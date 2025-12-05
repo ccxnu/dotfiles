8e77a0add6ab1346abde4d582feb1df3335f3268
ccxnu <108037606+ccxnu@users.noreply.github.com>
2025-12-04 21:08:59 -0500

    fix: put brackets to now prompt twice on history

--- bash/.config/bash/prompt.sh
+++ bash/.config/bash/prompt.sh
@@ -1,10 +1,10 @@
 # Color
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-BLUE="\033[0;34m"
-MAGENTA="\033[0;35m"
-CYAN="\033[0;36m"
-DEFAULT="\033[0m"
+RED='\[\033[0;31m\]'
+GREEN='\[\033[0;32m\]'
+BLUE='\[\033[0;34m\]'
+MAGENTA='\[\033[0;35m\]'
+CYAN='\[\033[0;36m\]'
+DEFAULT='\[\033[0m\]'
 
 # Simple Prompt
 promptcmd() {
